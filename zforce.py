0e9f48c6f2263cc2cf62ebf33bcf30710c0822c8
vitorlima <vitor_lp@yahoo.com>
2015-05-04 10:55:52 -0300

    Refactoring: created functions try_word, try_lowercase, try_uppercase, etc

--- zforce.py
+++ zforce.py
@@ -32,16 +32,25 @@ def bf_extract(zfile, password):
         raise InvalidZip("invalid zip file: " + zfile)
 
     return res
+
+def try_word(word, zip_file):
+    if bf_extract(zip_file, word):
+        print "The password is " + word
+        return True
+    return False
  
 def find_password(list_file, zip_file, mode):
     found = find_password_simple(list_file, zip_file)
     if not found and mode > 0:
-        found = find_password_with_variations(list_file, zip_file)
+        found = find_password_with_variations(zip_file)
     if not found and mode > 1:
-        find_password_with_combinations(list_file, zip_file)
+        found = find_password_with_combinations(list_file, zip_file)
+    if not found:
+        print "Password not found"
 
+
+# This function just try the words inside list_file as passwords for the zip_file
 words = []
-
 def find_password_simple(list_file, zip_file):
     try:
         file = open(list_file)
@@ -55,9 +64,7 @@ def find_password_simple(list_file, zip_file):
 
             try:
                 words.append(line)
-                if bf_extract(zip_file, line):
-                    print "The password is " + line
-                    found = True
+                found = try_word(line, zip_file)
 
             except InvalidZip:
                 break
@@ -66,39 +73,62 @@ def find_password_simple(list_file, zip_file):
     except IOError:
         return False
 
-def find_password_with_variations (list_file, zip_file):
-    variations = []
+#   This function try some variations of the words inside the list_file as
+# passwords for the zip_file.
+#        I - try uppercased versions of the words
+#        II - try lowercased versions of the words
+#        III - try change just some letters to uppercase
+#        IV - try replace some letters by another symbols (as E by 3, or k by |<, etc)
+variations = []
+def find_password_with_variations (zip_file):
     for word in words:
         # try all letters in uppercase
-        if not word.isupper():
-            up = word.upper()
-            words.append(up)
-            if bf_extract(zip_file, up):
-                print "The password is " + up
-                return True
-        # try all letters in lower
-        if not word.islower():
-            lower = word.lower()
-            words.append(lower)
-            if bf_extract(zip_file, lower):
-                print "The password is " + lower
-                return True
-        # try first letters uppercase
-        if not word.istitle():
-            title = word.title()
-            words.append(title)
-            if bf_extract(zip_file, title):
-                print "The password is " + title
-                return True
-        # try replace some chars (for example: teste --> t3st3)
-        words.append(word.replace("o", "0").replace("A", "4").replace("l", "1").replace("e", "3").replace("E", "3"))
-        r = words[-1]
-        if bf_extract(zip_file, r):
-            print "The password is " + r
+        found = try_uppercase(word, zip_file)
+        if not found:
+            found = try_lowercase(word, zip_file)
+        if not found:
+            found = try_some_uppercase(word, zip_file)
+        if not found:
+            found = try_simple_replacements(word, zip_file)
+            simple_replacements = variations[-1]
+        if not found:
+            found = try_more_replacements(word, zip_file)
+        if not found:
+            found = try_more_replacements(simple_replacements, zip_file)
+        if found:
             return True
-
     return False
 
+# try all letters in uppercase
+def try_uppercase(word, zip_file): 
+    up = word.upper()
+    if not word.isupper():
+        variations.append(up)
+    return try_word(up, zip_file)
+
+# try all letters in lower
+def try_lowercase(word, zip_file):
+    lower = word.lower()
+    if not word.islower():
+        variations.append(lower)
+    return try_word(lower, zip_file)
+
+# try first letters uppercase
+def try_some_uppercase(word, zip_file):
+    title = word.title()
+    if not word.istitle():
+        variations.append(title)
+    return try_word(title, zip_file)
+
+# try replace some chars (for example: teste --> t3st3)
+def try_simple_replacements(word, zip_file):
+    variations.append(word.replace("o", "0").replace("A", "4").replace("l", "1").replace("e", "3").replace("E", "3"))
+    return try_word(variations[-1], zip_file)
+
+# try replace some chars 
+def try_more_replacements(word, zip_file):
+    variations.append(word.replace("k", "|<").replace("P", "|>").replace("p", "|>").replace("S", "5").replace("s", "5"))
+    return try_word(variations[-1], zip_file)
 
 def find_password_with_combinations (list_file, zip_file):
     print "combinations"
