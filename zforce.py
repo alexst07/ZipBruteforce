e37aa10ae7cf4388ae480297ca6ea95c7f1b20c4
vitorlima <vitor_lp@yahoo.com>
2015-05-04 14:18:44 -0300

    Created functions to try combinations of the words and theirs variations

--- zforce.py
+++ zforce.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
 #
-# Task 04: Develop a program that does a
+#    Task 04: Develop a program that does a
 # bruteforce attack to find the password
 # of a password protected ZIP file.
+#
+#    You can use the -m option to set the level
+# of the attack: the default is zero and it will
+# just try the words that are in the file. The
+# level one also try some variations of these
+# words and the level two will also try some
+# combinations of these words and variations.
 #
 # Authors: Alex Silva Torres, RA 161939
 #          Hilder Vitor Lima Pereira, RA 161440
 
 import zipfile
 import optparse
+import itertools
+from random import randint
 
 class InvalidZip(Exception):
      def __init__(self, value):
@@ -35,7 +44,7 @@ def bf_extract(zfile, password):
 
 def try_word(word, zip_file):
     if bf_extract(zip_file, word):
-        print "The password is " + word
+        print "****  The password is " + word
         return True
     return False
  
@@ -44,9 +53,9 @@ def find_password(list_file, zip_file, mode):
     if not found and mode > 0:
         found = find_password_with_variations(zip_file)
     if not found and mode > 1:
-        found = find_password_with_combinations(list_file, zip_file)
+        found = find_password_with_combinations(zip_file)
     if not found:
-        print "Password not found"
+        print "****   Password not found"
 
 
 # This function just try the words inside list_file as passwords for the zip_file
@@ -73,6 +82,7 @@ def find_password_simple(list_file, zip_file):
     except IOError:
         return False
 
+#   Mode 1
 #   This function try some variations of the words inside the list_file as
 # passwords for the zip_file.
 #        I - try uppercased versions of the words
@@ -130,10 +140,84 @@ def try_more_replacements(word, zip_file):
     variations.append(word.replace("k", "|<").replace("P", "|>").replace("p", "|>").replace("S", "5").replace("s", "5"))
     return try_word(variations[-1], zip_file)
 
-def find_password_with_combinations (list_file, zip_file):
-    print "combinations"
+
+#   Mode 2
+#   This function try some combinations of the words and their variations as
+# passwords for the zip_file.
+#        I - try some sequences of words separated by white spaces
+#        II - try some sequences of words separated by underscore
+#        III - try just the first letters of each word in these sequences
+#        IV - try concatenate the words and theirs variations with some numbers (as years, birthdays, etc)
+def find_password_with_combinations (zip_file):
+    words_and_variations = words + variations
+    print "Combining with some years"
+    if try_concat_years(words_and_variations, zip_file):
+        return True
+
+    print "Combining with some birthdays"
+    if try_concat_birthdays(words_and_variations, zip_file):
+        return True
+    # try to combine the words the variations
+    list1 = words + variations
+    list2 = itertools.chain(words, variations)
+    print "Combining amoung themselves"
+    for word0 in words_and_variations:
+        for word1 in list1:
+            # using two words
+            if try_phrase_and_first_letters(word0 + " " + word1, zip_file):
+                return True
+            if try_word(word0 + "_" + word1, zip_file):
+                return True
+            for word2 in list2:
+                # using three words
+                phrase = word0 + " " + word1 + " " + word2
+                if try_phrase_and_first_letters(phrase, zip_file):
+                    return True
+                if try_word(word0 + "_" + word1 + "_" + word2, zip_file):
+                    return True
+
     return False
 
+# try the received phrase and also a word with the initial letter of each word of this phrase.
+#   For example:  if phrase == "This is my Password", then it tries "TIMP" too
+#                 if phrase == "Dad's name", then it tries "DN" too
+def try_phrase_and_first_letters(phrase, zip_file):
+    if try_word(phrase, zip_file):
+        return True
+    first_letters = [ i[0].upper() for i in phrase.split() ];
+    if try_word("".join(first_letters), zip_file):
+        return True
+    return False
+
+def try_concat_years(list_words, zip_file):
+    for word in list_words:
+        for year in range(1980, 2016):
+            if try_word(word + str(year), zip_file):
+                return True
+            if try_word(str(year) + word, zip_file):
+                return True
+
+    return False
+
+def try_concat_birthdays(list_words, zip_file):
+    for month in ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]:
+        for day in range(1, 32):
+            for year in range(1975, 2000):
+                if randint(0, 9) == 7 :
+                    bday = str(month) + "-" + str(day) + "-" + str(year)
+                    for word in list_words:
+                        if try_word(word + bday, zip_file):
+                            return True
+                        if try_word(bday + word, zip_file):
+                            return True
+    return False
+
+
+
+
+##########################
+#       main function
+##########################
 def main():
     p = optparse.OptionParser("usage: %prog -l <dic name> -f <zip name> [-m <integer>]")
     p.add_option('-l', help="Filename of the dictionary is required",
@@ -141,7 +225,7 @@ def main():
     p.add_option('-f', help="Filename of the zip is required",
                  dest="zip_file")
     p.add_option('-m', help="Mode of operation should be a integer:      0 -> default: just try the words in the file (fast)   1 -> try some variations of the words    2 -> try several variations and combinations of the words (expensive) ",
-	             type="int",
+                 type="int",
                  dest="mode")
 
 
